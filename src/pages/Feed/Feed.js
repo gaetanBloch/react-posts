653ab68f28afebc2f58d83791384a76c0156ceef
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-24 12:25:38 +0200

    Add a method to add A post at the top of the list of posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -46,6 +46,20 @@ class Feed extends Component {
     openSocket(URL_PREFIX);
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
