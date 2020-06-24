c35864f9e90097cb4486ee9c7a076c4f8c1bb2e2
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-24 12:21:39 +0200

    Connect to socket.io as a client

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -41,6 +42,8 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+
+    openSocket(URL_PREFIX);
   }
 
   loadPosts = direction => {
